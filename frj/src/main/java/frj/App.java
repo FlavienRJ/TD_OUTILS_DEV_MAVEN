3f4a9b66dc316ba36c566444d20cab8f4cbd4333
Ronteix-Jacquet Flavien <flavien.trade18@gmail.com>
2017-04-12 14:59:30 +0200

    Ajout de CSVWriter à partir du code fait à la maison

--- frj/src/main/java/frj/App.java
+++ frj/src/main/java/frj/App.java
@@ -7,9 +7,14 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
 
 import java.util.Iterator;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 /**
  * Hello world!
@@ -24,7 +29,7 @@ public class App
         //assertEquals(5, monApp.max(4, 5));
         //assertEquals(4, monApp.max(5, 4));
         
-        int Max = 0;
+        
         try
         {
 	        CSVReader reader = new CSVReader(new FileReader("data.csv"));
@@ -33,6 +38,8 @@ public class App
 		        List<String[]> myEntries = reader.readAll();
 		        Iterator<String[]> iterator = myEntries.iterator();
 		        iterator.next(); 
+		        final int Max = 0;
+		        int monMax = 0;
 		         
 		        while (iterator.hasNext()) 
 		        {
@@ -41,9 +48,36 @@ public class App
 		            for(int i=0; i<entree.length; i++)
 		            {
 		            	System.out.println("Nombre courant : "+Integer.parseInt(entree[i]));
-		                Max = monApp.max(Max, Integer.parseInt(entree[i]));
-		
+		                monMax = monApp.max(Max, Integer.parseInt(entree[i]));
+		                
 		            }
+		            
+		        }
+		        //Max = monMax;
+		        System.out.println("Nombre max : "+Max);
+		        
+		        while(iterator.hasNext())
+		        {
+		            String[] entree = iterator.next();
+		            List list       = Arrays.asList(entree);
+		             
+		            Vector<String> out = new Vector<String>();
+		            Predicate predicat = new Predicate()
+		            {
+		                public boolean evaluate(Object object)
+		                {
+		                    try
+		                    {
+		                        return Integer.parseInt((String) object) > Max;
+		                    }
+		                    catch (java.lang.NumberFormatException e)
+		                    {
+		                        return false;
+		                    }
+		                }
+		            };
+		            CollectionUtils.select(list, predicat, out);
+		            System.out.println("OUT : " + out);
 		        }
             }
 	        
@@ -56,7 +90,9 @@ public class App
         {
             System.out.println(ex.getMessage());
         }
-        System.out.println("Nombre max : "+Max);
+        
+        
+        
         
     }
     public int max(int a, int b)
@@ -65,129 +101,3 @@ public class App
     	//return a; //Pourquoi on a laissé le stagiaire toucher à ça ?
     }
 }
-/*
- Travail de preparation
-package frj;
-
-import java.io.IOException;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
- 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Vector;
- 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Predicate;
- 
-import java.util.Iterator;
-import com.opencsv.CSVReader;
- 
-public class App 
-{   
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("Ceci est ma modification");
-        App monApp = new App();
-        System.out.println(monApp.max(4,5));
-         
-        final int monmax = 0;
-        //int monmax = 0;
-        try
-        {
-            CSVReader reader = new CSVReader(new FileReader("data.csv"));
-            try
-            {
-                List<String[]> myEntries = reader.readAll();
-                Iterator<String[]> iterator = myEntries.iterator();
-                iterator.next(); 
-                 
-                /*
-                while (iterator.hasNext()) 
-                {
-                    String[] entree = iterator.next();
-                     
-                    for(int i=0; i<entree.length; i++)
-                    {
-                        try
-                        {
-                            if(Integer.parseInt(entree[i]) > monmax)
-                            {
-                                System.out.println("Nombre courant : "+Integer.parseInt(entree[i]));
-                                monmax = Integer.parseInt(entree[i]);
-                            }
-                        }
-                        catch (java.lang.NumberFormatException e)
-                        {
-                            continue;
-                        }
-                    }
-                }
-                System.out.println("Age maximum : "+monmax);
-                */
-                 
-                /*
-                while (iterator.hasNext()) 
-                {
-                    String[] entree = iterator.next();
-                     
-                    for(int i=0; i<entree.length; i++)
-                    {
-                        try
-                        {
-                            System.out.println("Nombre courant : "+Integer.parseInt(entree[i]));
-                            monmax = monApp.max(monmax, Integer.parseInt(entree[i]));
-                        }
-                        catch (java.lang.NumberFormatException e)
-                        {
-                            continue;
-                        }
-                    }                    
-                }
-                System.out.println("Age maximum : "+monmax);
-                
-                 
-                while(iterator.hasNext())
-                {
-                    String[] entree = iterator.next();
-                    List list       = Arrays.asList(entree);
-                     
-                    Vector<String> out = new Vector<String>();
-                    Predicate predicat = new Predicate()
-                    {
-                        public boolean evaluate(Object object)
-                        {
-                            try
-                            {
-                                return Integer.parseInt((String) object) > monmax;
-                            }
-                            catch (java.lang.NumberFormatException e)
-                            {
-                                return false;
-                            }
-                        }
-                    };
-                    CollectionUtils.select(list, predicat, out);
-                    System.out.println("OUT : " + out);
-                }
-            }
- 
-            catch (java.io.IOException e)
-            {
-                System.out.println(e.getMessage());
-            }
-        }
-        catch (java.io.FileNotFoundException ex)  
-        {
-            System.out.println(ex.getMessage());
-        }
-    }
-     
-    public int max(int a, int b)
-    {
-        return a > b ? a : b;
-        //return a; // Un stagiaire est passe par la
-    }
-}*/
