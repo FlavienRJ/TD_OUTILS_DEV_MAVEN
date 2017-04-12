4e9924f628c440e272ff7b514fffaa9beeb1f023
Ronteix-Jacquet Flavien <flavien.trade18@gmail.com>
2017-04-12 14:45:21 +0200

    Ajout de la lecture du fichier csv + traitement + test unitaire + marelease-v2

--- frj/src/main/java/frj/App.java
+++ frj/src/main/java/frj/App.java
@@ -1,5 +1,16 @@
 package frj;
 
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+ 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import java.util.Iterator;
+import com.opencsv.CSVReader;
+
 /**
  * Hello world!
  *
@@ -9,9 +20,43 @@ public class App
     public static void main( String[] args )
     {
     	App monApp = new App();
-        System.out.println(monApp.max(4,5));
+        //System.out.println(monApp.max(4,5));
         //assertEquals(5, monApp.max(4, 5));
         //assertEquals(4, monApp.max(5, 4));
+        
+        int Max = 0;
+        try
+        {
+	        CSVReader reader = new CSVReader(new FileReader("data.csv"));
+	        try
+            {
+		        List<String[]> myEntries = reader.readAll();
+		        Iterator<String[]> iterator = myEntries.iterator();
+		        iterator.next(); 
+		         
+		        while (iterator.hasNext()) 
+		        {
+		            String[] entree = iterator.next();
+		             
+		            for(int i=0; i<entree.length; i++)
+		            {
+		            	System.out.println("Nombre courant : "+Integer.parseInt(entree[i]));
+		                Max = monApp.max(Max, Integer.parseInt(entree[i]));
+		
+		            }
+		        }
+            }
+	        
+            catch (java.io.IOException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+        catch (java.io.FileNotFoundException ex)  
+        {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Nombre max : "+Max);
         
     }
     public int max(int a, int b)
